cd1e1cf6ce5414e8ac7243d06421fdd746427b86
Mr.Ranjan <119514811+CoderTobo@users.noreply.github.com>
2024-07-16 14:33:35 +0000

    opertiond config done

---
+++ 01_Basics/04_Operation.js
@@ -0,0 +1,27 @@
+let value = 16
+ let negValue = -value
+ console.log(negValue);
+
+ // console.log(16+26);
+ // console.log(26-16);
+ // console.log(26*16)
+ // console.log(26**16);
+ // console.log(26/16);
+ // console.log(26%16);
+
+
+let str1 = "hello"
+let str2 = " Ranjan"
+
+let str3 = str1+str2
+console.log(str3);
+
+//console.group(+true);
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
