b49e524e12239d873c4cd93abc028a77860904de
Mr.Ranjan <119514811+CoderTobo@users.noreply.github.com>
2024-07-16 14:01:55 +0000

    conversion of datatypes

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+ let score = "Ranjan"
+
+ //console.log(typeof score);
+ //console.log(typeof(score));
+
+ let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+
+ //console.log(valueInNumber);
+
+ // "62" => 62
+ // "62abc" => NaN
+ // true => 1; false => 0;
+
+ let isLoggedIn = "Ranjan"
+
+ let booleanIsLoggedIn = Boolean(isLoggedIn)
+ console.log(booleanIsLoggedIn);
+
+ // 1 => true; 0 =>false
+ // "" => false
+ // "Ranjan" => true
+
+ let someNumber = 62
+
+ let stringNumber = String(someNumber)
+ console.log(stringNumber);
+ console.log(typeof stringNumber)
