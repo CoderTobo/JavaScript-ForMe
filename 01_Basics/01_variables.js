8df39649d657e9961718d7a4263c53657d1a71c6
Mr.Ranjan <119514811+CoderTobo@users.noreply.github.com>
2024-07-16 13:28:08 +0000

    container config done

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 2616
+let accountEmail = "ranjan@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+// accountId = 2 // not allowed
+
+accountEmail = "ry@ry.com"
+accountPassword = "11223344"
+accountCity = "uttar predesh"
+
+
+console.log(accountId);
+
+
+
+console.table([accountId , accountEmail , accountPassword , accountCity , accountState])
---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; //treats all JS code as newer version
+
+//alert( 3+ 3)// we are using nodejs, not browser
+
+console.log(3 + 3) // code readability should be high
+
+console.log("ranjan")
+
+let name = "ranjan"      // string datatype  (=>"")
+let age = 20             // number datatypes  (=>2 to power53)
+let isLoggedIn = false    // boolean datatype  (=> true or false)
+let state;
+
+// bigint
+// null => standalone value
+// undefined =>
+// symbol =>unique
+
+
+// object
+
+
+console.log(typeof age);
+console.log(typeof "ranjan");
+console.log(typeof false);
